13670d76cefa4fa7987b85754c0c4afb5bbb563f
LUOkJJ <2825452714@qq.com>
2018-10-11 15:29:15 +0800

    微服务

---
+++ hitao_gateway/src/main/java/com/hzitxx/MicoService.java
@@ -0,0 +1,16 @@
+package com.hzitxx;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+@EnableEurekaClient
+@SpringBootApplication
+@EnableFeignClients  //让feign起效果
+public class MicoService {
+	public static void main(String[] args) {
+		SpringApplication.run(MicoService.class, args);
+	}
+	
+}
